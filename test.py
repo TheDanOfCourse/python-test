a84acd31060a329af7a08b23763280b04ffa3156
Danilo <60882968+TheDanOfCourse@users.noreply.github.com>
2022-07-12 00:32:46 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-def solution():
-    string='aaaaaa'
+def solution(string):
     if string.islower() and string.isalpha():
         if 2 <= len(string) <= 300000: 
             split_string = [string[i:i+2] for i in range(0, len(string), 1)]
